fe9085d44b4c7a361b02001a77f17b0093f26ec9
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-08-19 15:37:51 +0200

    Les produits de la page d'accueil son lier à l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,5 @@
-const productImg = document.querySelector("img");
-const productName = document.querySelector(".productName");
-const productDescription = document.querySelector(".productDescription");
-
+const articleAccueil = document.querySelector("section.items a");
+const sectionItems = document.querySelector("#items");
 
 const getProducts = async () => {
     const res = await fetch("http://localhost:3000/api/products")
@@ -9,29 +7,28 @@ const getProducts = async () => {
     return data
 };
 
+
+const urlProducts = "http://127.0.0.1:5500/front/html/product.html";
+const url = new URL(urlProducts);
+
+
 getProducts().then(data => {
-    productName.textContent = data[0].name
-})
-
-/*const insertProducts = () => {
-    getProducts() 
-};
-
-insertProducts()*/
-
-/* 1
-const getProducts = () => {
-    fetch("http://localhost:3000/api/products")
-        .then(res => res.json())
-        .then(data => data)
-        .catch(e => console.log(e))
-};
-
-
-const insertProducts = async () => {
-    getProducts()
-    .then(productName.textContent = data[0].nom)
-};
-
-insertProducts()
-*/
+    for (i = 0; i < data.length; i++) {
+        const urlProduct = url.searchParams.get(data[i]._id)
+        if (i == 0){
+            document.querySelector(".productName").textContent = data[i].name
+            document.querySelector(".productDescription").textContent = data[i].description
+            document.querySelector("section.items img").src = data[i].imageUrl
+            document.querySelector("section.items a").href = urlProducts + "?id=" + data[i]._id
+            console.log(urlProduct)   
+            
+        } else {
+            const dupArticle = articleAccueil.cloneNode(true);
+            const articleItem = sectionItems.appendChild(dupArticle)
+            articleItem.querySelector(".productName").textContent = data[i].name
+            articleItem.querySelector(".productDescription").textContent = data[i].description
+            articleItem.querySelector("section.items img").src = data[i].imageUrl
+            articleItem.href = urlProducts + "?id=" + data[i]._id
+        }
+    }
+});
