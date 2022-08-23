45ad1013f32ccc05a410c12a49d8b327c9df9bf0
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-08-23 15:29:54 +0200

    La page produit est lier a l'API

--- front/js/product.js
+++ front/js/product.js
@@ -1,24 +1,45 @@
-const getProducts = async () => {
-    const res = await fetch("http://localhost:3000/api/products")
+const urlParams = new URLSearchParams(window.location.search);
+const idProduct = urlParams.get('id');
+
+const getProduct = async () => {
+    const res = await fetch("http://localhost:3000/api/products/" + idProduct)
     const data = await res.json()
-    return data
+    return data 
 };
 
-const params = window.location.search
 
-getProducts().then(data => {
-    for (i = 0; i < data.length; i++) {
-        if (params == "?id=" + data[i]._id){
-            document.querySelector("section.item img").src = data[i].imageUrl
-            document.querySelector("section.item h1").textContent = data[i].name
-            document.querySelector("section.item span#price").textContent = data[i].price
-            document.querySelector("section.item p#description").textContent = data[i].description
-            for (c = 0; c < data[i].colors.length; c++) {
-                const dupOption = document.querySelector("section.item option").cloneNode(true)
-                const optionColor = document.querySelector("select#colors").appendChild(dupOption)
-                optionColor.value = data[i].colors[c]
-                optionColor.textContent = data[i].colors[c]
-            }
-        }
+const titrePage = document.querySelector("head title");
+
+const item__img = document.querySelector("div.item__img");
+const img = document.createElement("img");
+const imgInser = item__img.appendChild(img);
+
+const nameProduct = document.querySelector("h1#title");
+
+const priceProduct = document.querySelector("span#price");
+
+const descriptionProduct = document.querySelector("p#description");
+
+const selectColor = document.querySelector("select#colors");
+const option = document.querySelector("select#colors option");
+
+getProduct().then(data => {
+    titrePage.textContent = data.name + " - " + data.description
+
+    img.src = data.imageUrl
+    img.alt = data.altTxt
+
+    nameProduct.textContent = data.name
+
+    priceProduct.textContent = data.price
+
+    descriptionProduct.textContent = data.description
+    
+    for(i = 0; i < data.colors.length; i++) {
+        const cloneOption = option.cloneNode(true)
+        const nouvelleColor = selectColor.appendChild(cloneOption)
+
+        nouvelleColor.value = data.colors[i]
+        nouvelleColor.textContent = data.colors[i]        
     }
-})
+});
--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,29 @@
-const articleAccueil = document.querySelector("section.items a");
-const sectionItems = document.querySelector("#items");
-
 const getProducts = async () => {
     const res = await fetch("http://localhost:3000/api/products")
     const data = await res.json()
     return data
 };
 
-
-
+const sectionItems = document.querySelector("#items");
 
 getProducts().then(data => {
+
+    let template = ''
+
     for (i = 0; i < data.length; i++) {
-        const urlProducts = "http://127.0.0.1:5500/front/html/product.html";
-        const urlProduct = urlProducts + "?id=" + data[i]._id
-        if (i == 0){
-            document.querySelector(".productName").textContent = data[i].name
-            document.querySelector(".productDescription").textContent = data[i].description
-            document.querySelector("section.items img").src = data[i].imageUrl
-            document.querySelector("section.items a").href = urlProduct 
-        } else {
-            const dupArticle = articleAccueil.cloneNode(true);
-            const articleItem = sectionItems.appendChild(dupArticle)
-            articleItem.querySelector(".productName").textContent = data[i].name
-            articleItem.querySelector(".productDescription").textContent = data[i].description
-            articleItem.querySelector("section.items img").src = data[i].imageUrl
-            articleItem.href = urlProduct
-        }
+
+        const product = data[i]
+
+        const articleItem =  `<a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>` ;
+
+          template += articleItem
     }
+    
+    sectionItems.innerHTML = template
 });
