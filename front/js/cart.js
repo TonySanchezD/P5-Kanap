69c4eff59547956a799f75269ae9c1454c5aaeda
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-10-12 14:38:23 +0200

    Bug mail valide résaulu

--- front/js/cart.js
+++ front/js/cart.js
@@ -203,7 +203,7 @@ form.addEventListener("submit", function (event) {
         contact[nameField] = valueField   
     }
 
-    function checkMail(mail) {
+    function checkMail(email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
